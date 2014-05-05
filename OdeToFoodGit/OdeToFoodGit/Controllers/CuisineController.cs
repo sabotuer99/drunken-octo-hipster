fe155f53940d77e40c5d9993d688455a6f859ed9
Troy Whorten <troy.whorten@wyo.gov>
2014-05-05 14:26:53 -0600

    Added 'Cuisine' controller

---
+++ OdeToFoodGit/OdeToFoodGit/Controllers/CuisineController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFoodGit.Controllers
+{
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+
+        public ActionResult Search(string name)
+        {
+
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+        }
+
+    }
+}
